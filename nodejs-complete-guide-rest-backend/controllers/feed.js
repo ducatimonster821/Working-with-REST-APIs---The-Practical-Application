b17f4a1c7d195b57c78e07163704c5bb34f3ca5a
ducatimonster821 <s.asusk56c@gmail.com>
2019-05-04 08:05:37 +0500

    lesson378

--- nodejs-complete-guide-rest-backend/controllers/feed.js
+++ nodejs-complete-guide-rest-backend/controllers/feed.js
@@ -43,12 +43,27 @@ exports.getPosts = (req, res, next) => {
     //     ]
     // });
 
+    console.log('req.query:', req.query);
+
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
     Post.find()
+        .countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find()
+                .skip((currentPage - 1) * perPage)
+                .limit(perPage);
+        })
         .then(posts => {
-            res.status(200).json({
-                message: 'Fetched posts successfully.',
-                posts: posts
-            });
+            res
+                .status(200)
+                .json({
+                    message: 'Fetched posts successfully.',
+                    posts: posts,
+                    totalItems: totalItems
+                });
         })
         .catch(err => {
             if (!err.statusCode) {
